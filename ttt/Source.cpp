8b40ac55504e8e2d18e8bbd72bd723ccf7c20292
ArtemPankreatin <clim.angel@yandex.ru>
2021-12-07 21:05:11 +0200

    надеюсь это сработает

--- ttt/Source.cpp
+++ ttt/Source.cpp
@@ -13,108 +13,142 @@ HANDLE hout;
 COORD c = { 33,7 };
 CONSOLE_CURSOR_INFO ci;
 int field[13][13];
-int game[3][3] = {0};
+int game[3][3] = { 0 };
 int hod = 1;
 
 int curX = 0;
 int curY = 0;
 bool flag = true;
-bool flag1 = true;
 int winner = 0;
- 
-void Game() 
-{
-	COORD move[3][3] = { {{35,9}, {39,9}, {43,9}}, {{35,13}, {39,13}, {43,13}}, {{35,17}, {39,17}, {43,17}} };
-	SetConsoleCursorPosition(hout, move[0][0]);
-	int x = _getch();
-	while (flag)
-	{
-		SetConsoleCursorPosition(hout, move[curX][curY]);
-		x = _getch();
+int x;
 
-		switch (x)
-		{
-		case 75:
-			if (curY >= 0 && curY <= 2) {
-				if (curY - 1 >= 0 && curY - 1 <= 2) 
-				{
-					curY--;
-					SetConsoleCursorPosition(hout, move[curY][curX]);
-				}
-			}
-			break;
-		case 77:
-			if (curY >= 0 && curY <= 2)
-				if (curY + 1 >= 0 && curY + 1 <= 2) 
-				{
-					curY++;
-					SetConsoleCursorPosition(hout, move[curY][curX]);
-				}
-			break;
-		case 80:
-			if (curX >= 0 && curX <= 2)
-				if (curX + 1 >= 0 && curX + 1 <= 2) 
-				{
-					curX++;
-					SetConsoleCursorPosition(hout, move[curY][curX]);
-				}
-			break;
-		case 72:
-			if (curX >= 0 && curX <= 2)
-				if (curX - 1 >= 0 && curX - 1 <= 2) 
-				{
-					curX--;
-					SetConsoleCursorPosition(hout, move[curY][curX]);
-				}
-			break;
-		case 32:
-			if (game[curX][curY] == 1) 
-			{
-				SetConsoleCursorPosition(hout, { 0,20 });
-				cout << "Fuck you leatherman!";
-			}
-			else 
-			{
-				cout << "X";
-				SetConsoleCursorPosition(hout, move[curY][curX]);
-				game[curX][curY] = 1;
-				hod = 2;
-			}
+void Bot_turn() {
+	bool bot_flag = true;
+	COORD move[3][3] = { {{35,9}, {39,9}, {43,9}}, {{35,13}, {39,13}, {43,13}}, {{35,17}, {39,17}, {43,17}} };
+	while (bot_flag) {
+		if (game[1][1] == 0) {
+			game[1][1] = 2;
+			SetConsoleCursorPosition(hout, move[1][1]);
+			cout << "O";
 			break;
 		}
-		check();
+		if (game[0][0] == 1 && game[0][1] == 1) {
+			if (game[0][2] == 0) {
+				game[0][2] = 2;
+				SetConsoleCursorPosition(hout, move[0][2]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[0][1] == 1 && game[0][2] == 1) {
+			if (game[0][0] == 0) {
+				game[0][0] = 2;
+				SetConsoleCursorPosition(hout, move[0][0]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[0][0] == 1 && game[0][2] == 1) {
+			if (game[0][1] == 0) {
+				game[0][1] = 2;
+				SetConsoleCursorPosition(hout, move[0][1]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[0][0] == 1 && game[1][0] == 1) {
+			if (game[2][0] == 0) {
+				game[2][0] = 2;
+				SetConsoleCursorPosition(hout, move[2][0]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[1][0] == 1 && game[2][0] == 1) {
+			if (game[0][0] == 0) {
+				game[0][0] = 2;
+				SetConsoleCursorPosition(hout, move[0][0]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[0][0] == 1 && game[2][0] == 1) {
+			if (game[1][0] == 0) {
+				game[1][0] = 2;
+				SetConsoleCursorPosition(hout, move[1][0]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[0][2] == 1 && game[1][2] == 1) {
+			if (game[2][2] == 0) {
+				game[2][2] = 2;
+				SetConsoleCursorPosition(hout, move[2][2]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[1][2] == 1 && game[2][2] == 1) {
+			if (game[0][2] == 0) {
+				game[0][2] = 2;
+				SetConsoleCursorPosition(hout, move[0][2]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[0][2] == 1 && game[2][2] == 1) {
+			if (game[1][2] == 0) {
+				game[1][2] = 2;
+				SetConsoleCursorPosition(hout, move[1][2]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[2][0] == 1 && game[2][1] == 1) {
+			if (game[2][2] == 0) {
+				game[2][2] = 2;
+				SetConsoleCursorPosition(hout, move[2][2]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[2][1] == 1 && game[2][2] == 1) {
+			if (game[2][0] == 0) {
+				game[2][0] = 2;
+				SetConsoleCursorPosition(hout, move[2][0]);
+				cout << "O";
+				break;
+			}
+		}
+		if (game[2][0] == 1 && game[2][2] == 1) {
+			if (game[2][1] == 0) {
+				game[2][1] = 2;
+				SetConsoleCursorPosition(hout, move[2][1]);
+				cout << "O";
+				break;
+			}
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (game[i][j] == 0) {
+					game[i][j] = 2;
+					SetConsoleCursorPosition(hout, move[i][j]);
+					cout << "O";
+					bot_flag = false;
+					break;
+				}
+			}
+			if (!bot_flag) break;
+		}
 	}
 }
 
-void check() 
-{
-	if (game[0][0] == game[0][1] && game[0][0] == game[0][2]) //первая горизонталь
-	{
-		if (game[0][0] == 1) 
-		{
-			flag = 0;
-			winner = 1;
-		}
-		else if (game[0][0] == 2) 
-		{
-			flag = 0;
-			winner = 2;
-		}
-	}
-	if (game[1][0] == game[1][1] && game[1][1] == game[1][2]) //вторая горизонталь
-	{
-		if (game[1][0] == 1) 
-		{
-			flag = 0;
-			winner = 1;
-		}
-		else if (game[1][0] == 2) 
-		{
-			flag = 0;
-			winner = 2;
-		}
-	}
-	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && game[2][0] == game[2][2]) //третья горизонталь
+void Player_turn() {
+	COORD move[3][3] = { {{35,9}, {39,9}, {43,9}}, {{35,13}, {39,13}, {43,13}}, {{35,17}, {39,17}, {43,17}} };
+
+
+	bool player_flag = true;
+	while (player_flag)
 	{
 		SetConsoleCursorPosition(hout, move[curX][curY]);
 		x = _getch();
@@ -133,11 +167,11 @@ void check()
 			if (curX - 1 >= 0 && curX - 1 <= 2) curX--;
 			break;
 		case 32:
-			if (game[curX][curY] == 1){
+			if (game[curX][curY] == 1) {
 				SetConsoleCursorPosition(hout, { 0,20 });
 				cout << "Wrong move!";
 			}
-			else{
+			else {
 				cout << "X";
 				game[curX][curY] = 1;
 				player_flag = 0;
@@ -145,10 +179,10 @@ void check()
 			break;
 		}
 	}
- }
-void Game() 
+}
+void Game()
 {
-	SetConsoleCursorPosition(hout, {35,9});
+	SetConsoleCursorPosition(hout, { 35,9 });
 	int x = _getch();
 	while (flag) {
 		Player_turn();
@@ -166,7 +200,7 @@ void Game()
 	SetConsoleCursorInfo(hout, &ci);
 }
 
-void Check() 
+void Check()
 {
 	for (int i = 0; i < 3; i++) {
 		if (game[0][i] == game[1][i] && game[1][i] == game[2][i]) {
@@ -193,11 +227,11 @@ void Check()
 		if (game[0][0] == 1) {
 			winner = 1;
 			flag = 0;
-		} 
+		}
 		else if (game[0][0] == 2) {
 			winner = 2;
 			flag = 0;
-		} 
+		}
 	}
 	if (game[2][0] == game[1][1] && game[1][1] == game[0][2]) //диагональ справа на лево
 	{
@@ -210,12 +244,6 @@ void Check()
 			flag = 0;
 		}
 	}
-	/*for(int i = 0; i<3; i++)
-		for(int j = 0; j<3; j++)
-			if (game[i][j] != 0) counter++;
-	if (counter == 9) flag = 0;
-	counter = 0;*/
-	
 }
 
 void Draw()
@@ -225,31 +253,31 @@ void Draw()
 		SetConsoleCursorPosition(hout, c);
 		for (int j = 0; j < 13; j++)
 		{
-			cout <<(char)field[i][j];
+			cout << (char)field[i][j];
 		}
 		c.Y++;
 	}
 }
 
-void Field(int arr[13][13])
+void Create_field()
 {
 	for (int i = 0; i < 13; i++)
 		for (int j = 0; j < 13; j++)
 		{
-			if (i == 0 && j % 4 == 0) arr[i][j] = 203; // ╦
-			else if (i == 12 && j % 4 == 0) arr[i][j] = 202; // ╩
-			else if (i % 4 == 0 && j % 4 != 0) arr[i][j] = 205; // ═
-			else if (i % 4 != 0 && j % 4 == 0) arr[i][j] = 186; // ║
-			else if (i % 4 == 0 && j % 4 == 0 && i != 0 && j != 0 && j != 12 && i != 12) arr[i][j] = 206; // ╬
-			else if (i % 4 == 0 && j == 0) arr[i][j] = 204; // ╠
-			else if (i % 4 == 0 && j == 12) arr[i][j] = 185; // ╣
-			else if (i % 4 != 0 && j % 4 != 0) arr[i][j] = 255; //space
+			if (i == 0 && j % 4 == 0) field[i][j] = 203; // ╦
+			else if (i == 12 && j % 4 == 0) field[i][j] = 202; // ╩
+			else if (i % 4 == 0 && j % 4 != 0) field[i][j] = 205; // ═
+			else if (i % 4 != 0 && j % 4 == 0) field[i][j] = 186; // ║
+			else if (i % 4 == 0 && j % 4 == 0 && i != 0 && j != 0 && j != 12 && i != 12) field[i][j] = 206; // ╬
+			else if (i % 4 == 0 && j == 0) field[i][j] = 204; // ╠
+			else if (i % 4 == 0 && j == 12) field[i][j] = 185; // ╣
+			else if (i % 4 != 0 && j % 4 != 0) field[i][j] = 255; //space
 		}
-		arr[0][0] = 201; // ╔
-		arr[0][12] = 187; // ╗
-		arr[12][0] = 200; // ╚
-		arr[12][12] = 188; // ╝
-	}
+	field[0][0] = 201; // ╔
+	field[0][12] = 187; // ╗
+	field[12][0] = 200; // ╚
+	field[12][12] = 188; // ╝
+	Draw();
 }
 
 int main()
@@ -259,15 +287,12 @@ int main()
 	ci.bVisible = true;
 	SetConsoleCursorInfo(hout, &ci);
 	SetConsoleCursorPosition(hout, c);
+	Create_field();
 
-	Field(field);
-	Draw();
 	Game();
-	/*for (int i = 0; i < 256; i++)
-	{
-		cout << i << '\t' << (char)i << "\n\n";
-	}*/
-	
+
+
+
 	cout << "\n\n" << endl;
 	system("pause");
 }
